755a9971be1eae21f90243ba4fa42e0df6d37f10
김영균 <kyoungyun98@naver.com>
2021-05-07 22:40:31 +0900

    feat: add home api
    스토리, 피드 불러오는 api 추가

--- src/components/api.js
+++ src/components/api.js
@@ -6,10 +6,15 @@ const callApi = async (method , path, data) => {
     if(method === 'post'){
         return axios.post(fullUrl, data);
     }
+    else if(method === 'get'){
+        return axios.get(fullUrl, data);
+    }
 }
 export default{
     createAccount: form => callApi('post', '/signUp/v1/members', form),
     login: form => callApi('post', '/signIn/v1/members', form),
     getProfile: form => callApi('get',' ',form),
     getFeedList : form => callApi('get',' ',form),
+    getStoryList: form => callApi('get',' ',form),
+    getHomeFeedList : form => callApi('get',' ',form),
 }
